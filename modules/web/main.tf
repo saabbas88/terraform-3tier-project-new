eae1063e756726ac3a772d29892598a6f63f81e3
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-11-03 14:30:45 +0530

    Update main.tf

--- modules/web/main.tf
+++ modules/web/main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web" {
   image_id      = var.ami_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.sg_ids
-  user_data              = "${file("apache.sh")}"
+  user_data     = base64encode(var.user_data)
 }
 
 resource "aws_autoscaling_group" "web" {
