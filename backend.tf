b1d4dbd670e19968daeb6900ced55aeff4374343
saabbas88 <85310420+saabbas88@users.noreply.github.com>
2025-11-03 13:37:07 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -6,7 +6,7 @@ terraform {
     bucket         = "abbz-tfstate-bucket-123"
     key            = "3tier/prod/terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "dynamodb-terraform-state-lock"
+    dynamodb_table = "dynamodbb-terraform-state-lock"
     encrypt        = true
   }
 }
